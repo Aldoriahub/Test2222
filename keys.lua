5158e696f981d320c55e9e3d8760b983f26a4c69
VirtualHub <lukasvonbraun@gmail.com>
2025-02-15 19:29:28 +0100

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,4 +1,11 @@
 return {
-    "VirtualHub_335353535",
-   
+    keys = {
+        "VirtualHub_335353535",
+        "Another_Valid_Key_123",
+        "TestKey_456"
+    },
+    devkeys = {
+        "DevKey_999",
+        "SpecialAdminKey_777"
+    }
 }
